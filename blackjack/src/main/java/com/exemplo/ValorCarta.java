fbf27003c7c65280d2fb55bc2cd3505e189b406d
rodrigoschotkis <rodrigoschotkis@gmail.com>
2026-08-28 17:22:46 +0000

    add card values enum

--- blackjack/src/main/java/com/exemplo/ValorCarta.java
+++ blackjack/src/main/java/com/exemplo/ValorCarta.java
@@ -1,5 +1,17 @@
 package com.exemplo;
 
 public enum ValorCarta {
-
+    AS,
+    DOIS,
+    TRES,
+    QUATRO,
+    CINCO,
+    SEIS,
+    SETE,
+    OITO,
+    NOVE,
+    DEZ,
+    VALETE,
+    DAMA,
+    REI
 }
